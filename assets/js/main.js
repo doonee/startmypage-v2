550c145995ee69f16360d9118b1cf24287c8bb06
doonee <doonee@outlook.com>
2022-11-08 11:52:20 +0900

    masonry

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ var elem = document.querySelector(".grid");
 var msnry = new Masonry(elem, {
   //options
   //columnWidth: 10,
-  columnWidth: 50,
+  //   columnWidth: 50,
+  gutter: 10,
   itemSelector: ".grid-item",
   fitWidth: true, //중앙정렬!!
   horizontalOrder: true, //순서지킴
