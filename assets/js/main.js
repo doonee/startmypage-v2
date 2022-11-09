5e1f0b19a9a98e0a29c23268dee1c623ecc1e598
doonee <doonee@outlook.com>
2022-11-09 12:38:55 +0900

    added long-press-2.4.4 module

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,5 @@
-// Masonry
-var elem = document.querySelector(".grid");
-var msnry = new Masonry(elem, {
-  //options
-  //columnWidth: 10,
-  //   columnWidth: 50,
-  gutter: 10,
-  itemSelector: ".grid-item",
-  fitWidth: true, //중앙정렬!!
-  horizontalOrder: true, //순서지킴
-  //transitionDuration: "0.2s",
-});
-
 // Modal
-const showBookmarkModal = () => {
+const showBookmarkModal = (e) => {
   var groupModal = new bootstrap.Modal(document.getElementById("newModal"), {});
   document
     .getElementById("newModal")
@@ -20,3 +7,26 @@ const showBookmarkModal = () => {
   groupModal.show();
 };
 //showBookmarkModal();
+
+window.onload = function () {
+  // Masonry
+  var elem = document.querySelector(".grid");
+  var msnry = new Masonry(elem, {
+    gutter: 10,
+    itemSelector: ".grid-item",
+    fitWidth: true, //중앙정렬!!
+    horizontalOrder: true, //순서지킴
+  });
+
+  // grab the element
+  var el = document.querySelectorAll(".grid > .grid-item > p");
+  console.log(el);
+  el.forEach((el) => {
+    el.addEventListener("long-press", function (e) {
+      // stop the event from bubbling up
+      e.preventDefault();
+      console.log(e.target);
+      showBookmarkModal();
+    });
+  });
+};
