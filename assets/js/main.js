bb4ba214cb4d186f1be912aac999a862130fcca3
doonee <d2607@naver.com>
2022-11-10 14:30:20 +0900

    removed long-press, added '1' before bookmark title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,34 @@
 // Modal
 const showBookmarkModal = (e) => {
-  var groupModal = new bootstrap.Modal(document.getElementById("newModal"), {});
-  document
-    .getElementById("newModal")
-    .classList.add("animate__animated", "animate__pulse");
-  groupModal.show();
+    var groupModal = new bootstrap.Modal(
+        document.getElementById("newModal"),
+        {}
+    );
+    document
+        .getElementById("newModal")
+        .classList.add("animate__animated", "animate__pulse");
+    groupModal.show();
 };
 //showBookmarkModal();
 
 window.onload = function () {
-  // Masonry
-  var elem = document.querySelector(".grid");
-  var msnry = new Masonry(elem, {
-    gutter: 10,
-    itemSelector: ".grid-item",
-    fitWidth: true, //중앙정렬!!
-    horizontalOrder: true, //순서지킴
-  });
-
-  // grab the element
-  var el = document.querySelectorAll(".grid > .grid-item > p");
-  el.forEach((el) => {
-    el.addEventListener("long-press", function (e) {
-      // stop the event from bubbling up
-      // 마우스 오른쪽 메뉴 막을려면 body에 oncontextmenu="return false" 추가 필요!
-      e.preventDefault();
-      console.log(e.target);
-      showBookmarkModal();
+    // Masonry
+    var elem = document.querySelector(".grid");
+    var msnry = new Masonry(elem, {
+        gutter: 10,
+        itemSelector: ".grid-item",
+        fitWidth: true, //중앙정렬!!
+        horizontalOrder: true, //순서지킴
     });
-  });
 
-  // 마우스 드래그, 우클릭 금지
-  // body 에도 추가 해줘야 함.
-  document.oncontextmenu = function () {
-    return false;
-  };
+    // grab the element
+    var el = document.querySelectorAll(".grid > .grid-item > p > i");
+    el.forEach((el) => {
+        el.addEventListener("click", function (e) {
+            e.preventDefault();
+            console.log(e.target);
+            e.target.parentElement.style.fontWeight = "bold";
+            showBookmarkModal();
+        });
+    });
 };
